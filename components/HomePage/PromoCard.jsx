444be6694ee2d5b3e0901e5967f4660af59391c4
hooman734 <hesamyan@outlook.com>
2021-02-12 03:42:35 +0400

    debugged

--- components/HomePage/PromoCard.jsx
+++ components/HomePage/PromoCard.jsx
@@ -30,7 +30,7 @@ const PromoCardCP = props => {
                         <Row className="justify-content-center">
                             <Col className="col-2">{''}</Col>
                             <Col className="col-auto">
-                                <Button className="px-auto" variant="danger">{''}<i className="fas fa-shopping-cart"/> Add </Button>
+                                <Button className="px-auto" variant="danger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/></svg> <span className="text-dark"> Add</span></Button>
                             </Col>
                             <Col className="col-2">{''}</Col>
                         </Row>
